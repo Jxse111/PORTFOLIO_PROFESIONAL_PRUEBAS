cbafb58de52a5cd65e261b3dd1407a5a23a86ffe
Jxse111 <josemartinezestrada111@gmail.com>
2025-10-20 17:36:45 +0200

    ajustes varios para subirlo a servidor

--- src/components/ChatModal.tsx
+++ src/components/ChatModal.tsx
@@ -188,9 +188,7 @@ export default function ChatModal({ onClose }: ChatModalProps) {
                 <div className="message-content">
                   <div className="message-bubble">
                     <p>{msg.text}</p>
-                    {msg.id === 'welcome' && (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-traffic-cone"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20l16 0" /><path d="M9.4 10l5.2 0" /><path d="M7.8 15l8.4 0" /><path d="M6 20l5 -15h2l5 15" /></svg>
-                    )}
+                    {msg.id === 'welcome'}
                   </div>
                   <div className="message-time">
                     {formatTime(msg.timestamp)}
